3443bf1f13aeadd5fc1ff180928eb57dc18a4647
Francois B <shakasan@sirenacorp.be>
2018-05-22 19:28:29 +0200

    steam headless install fixed

--- install_functions.sh
+++ install_functions.sh
@@ -166,14 +166,9 @@ function installGamesMenu () {
 # Steam (headless)
 #
 function installSteam () {
-  runCmd "echo steam	steam/purge	note | sudo debconf-set-selections" \
-        "accepting steam licence"
-
-  runCmd "echo steam	steam/license	note | sudo debconf-set-selections" \
-          "accepting steam licence 2/3"
-
-  runCmd "echo steam	steam/question	select	I AGREE | sudo debconf-set-selections" \
-          "accepting steam licence 3/3"
+  runCmd "echo steam steam/purge \"note\" | sudo debconf-set-selections \"accepting steam licence\""
+  runCmd "echo steam steam/license \"note\" | sudo debconf-set-selections \"accepting steam licence 2/3\""
+  runCmd "echo steam steam/question select \"I AGREE\" | sudo debconf-set-selections \"accepting steam licence 3/3\""
   installPackage apt steam
 }
 
@@ -181,14 +176,6 @@ function installSteam () {
 # Steam (Menu)
 #
 function installSteamMenu () {
-  runCmd "echo steam	steam/purge	note | sudo debconf-set-selections" \
-        "accepting steam licence"
-
-  runCmd "echo steam	steam/license	note | sudo debconf-set-selections" \
-          "accepting steam licence 2/3"
-
-  runCmd "echo steam	steam/question	select	I AGREE | sudo debconf-set-selections" \
-          "accepting steam licence 3/3"
   installPackage apt steam
 }
 
