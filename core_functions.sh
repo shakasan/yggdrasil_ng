74fbe087f85a3a3d055820aea34351ba4334afd8
Francois B. (Makoto) <francois@makotonoblog.be>
2020-07-04 19:48:42 +0200

    .

--- core_functions.sh
+++ core_functions.sh
@@ -621,6 +621,7 @@ function usage () {
   printf " "$BOLDVERT"-t"$NORMAL" : install TLP for Laptops and low energy usage\n"
   printf " "$BOLDVERT"-k"$NORMAL" : install HWE (Ubuntu's Hardware Enablement Stack)(newer kernel+xorg)\n"
   printf " "$BOLDVERT"-g"$NORMAL" : install/unlock SNAP + Store (not recommanded ;-))\n"
+  printf " "$BOLDVERT"-j"$NORMAL" : install/enable unattended-upgrades (security auto update))\n"
   printf " "$BOLDVERT"-u"$NORMAL" : update system (apt,snap,...)\n"
   printf " "$BOLDVERT"-p"$NORMAL" : clean useless packages\n"
 	printf " "$BOLDVERT"-v"$NORMAL" : show verison number\n"
